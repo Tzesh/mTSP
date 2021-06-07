e8025bbbff2b338dd08d1aaf60228872aad4f7e3
Uğur Dindar <55401127+Tzesh@users.noreply.github.com>
2021-06-07 23:37:02 +0300

    number of iterations -i parameter has been added

--- src/main/java/edu/anadolu/App.java
+++ src/main/java/edu/anadolu/App.java
@@ -26,12 +26,12 @@ public class App {
             return;
         }
 
-        if (params.getNumSalesmen() < 1 || params.getNumDepots() < 1) {
-            System.out.println("Number of salesmen and number of depots should be a positive integer value.");
+        if (params.getNumSalesmen() < 1 || params.getNumDepots() < 1 || params.getNumberOfIterations() < 1) {
+            System.out.println("Number of salesmen, number of iterations, and number of depots should be a positive integer value.");
             System.exit(0);
         }
 
-        IntStream.range(0, 1).parallel().forEach(a -> {
+        IntStream.range(0, params.getNumberOfIterations()).parallel().forEach(a -> {
             doAllSolutions(params);
         });
 
@@ -43,25 +43,25 @@ public class App {
         System.out.println("**Random Solution without Heuristics**");
         AtomicReference<mTSP> mTSP = new AtomicReference<>(new mTSP(params.getNumDepots(), params.getNumSalesmen(), Approach.RANDOM, false));
         mTSP.get().currentSolution.print(params.getNumSalesmen(), params.getVerbose(), false, false);
-        AtomicReference<edu.anadolu.core.mTSP> bestmTSP = mTSP;
+        AtomicReference<edu.anadolu.core.mTSP> best = mTSP;
 
         System.out.println("\n**Random Solution with Heuristics**");
         mTSP.set(new mTSP(params.getNumDepots(), params.getNumSalesmen(), Approach.RANDOM, true));
         mTSP.get().currentSolution.print(params.getNumSalesmen(), params.getVerbose(), false, true);
-        if (mTSP.get().currentSolution.cost < bestmTSP.get().currentSolution.cost) bestmTSP = mTSP;
+        if (mTSP.get().currentSolution.cost < best.get().currentSolution.cost) best = mTSP;
 
         System.out.println("\n**NN Solution without Heuristics**");
         mTSP.set(new mTSP(params.getNumDepots(), params.getNumSalesmen(), Approach.NN, false));
         mTSP.get().currentSolution.print(params.getNumSalesmen(), params.getVerbose(), false, false);
-        if (mTSP.get().currentSolution.cost < bestmTSP.get().currentSolution.cost) bestmTSP = mTSP;
+        if (mTSP.get().currentSolution.cost < best.get().currentSolution.cost) best = mTSP;
 
         System.out.println("\n**NN Solution with Heuristics**");
         mTSP.set(new mTSP(params.getNumDepots(), params.getNumSalesmen(), Approach.NN, true));
         mTSP.get().currentSolution.print(params.getNumSalesmen(), params.getVerbose(), false, true);
 
-        if (bestmTSP.get().currentSolution.cost < minCost[0].get()) {
-            minCost[0] = new AtomicInteger(bestmTSP.get().currentSolution.cost);
-            best = bestmTSP;
+        if (best.get().currentSolution.cost < minCost[0].get()) {
+            minCost[0] = new AtomicInteger(best.get().currentSolution.cost);
+            App.best = best;
         }
     }
 }
--- src/main/java/edu/anadolu/utils/Params.java
+++ src/main/java/edu/anadolu/utils/Params.java
@@ -13,6 +13,9 @@ public interface Params {
     @Option(description = "use city names when displaying/printing", shortName = "v", longName = "verbose")
     boolean getVerbose();
 
+    @Option(description = "Number of iterations (4 different solutions per each)", shortName = "i", longName = "iteration", defaultValue = "1")
+    int getNumberOfIterations();
+
     @Option(helpRequest = true, description = "display help", shortName = "h")
     boolean getHelp();
 
